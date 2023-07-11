d843159474baf9f98a9c9a7646b198cddc5752fc
Your Name <you@example.com>
2023-07-11 09:48:31 +0100

    name list is now displayed

--- src/components/BabyNamesList.tsx
+++ src/components/BabyNamesList.tsx
@@ -1,5 +1,37 @@
 import React from "react";
+import namesData from "../data/NamesData.json";
+
+
 
 export default function BabyNames(): JSX.Element {
-  return <></>;
+    
+  function getGirlsName(arr: object[]) {
+    const girlsNameArr = [];
+    
+    for (const eachName of namesData) {
+         if(eachName.sex === "f"){
+            girlsNameArr.push(eachName.name)}
+            
+  }
+  return girlsNameArr;
+  }
+  function getBoysName(arr: object[]) {
+    const boysNameArr = [];
+    
+    for (const eachName of namesData) {
+         if(eachName.sex === "m"){
+            boysNameArr.push(eachName.name)}
+            
+  }
+  return boysNameArr;
+  }
+
+  return (
+    <>
+      
+        <p >{getGirlsName(namesData).sort()}</p>
+        <p >{getBoysName(namesData).sort()}</p>
+      
+    </>
+  );
 }
